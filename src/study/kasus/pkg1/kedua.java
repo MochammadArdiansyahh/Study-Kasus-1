cb007135c31414790e27c05828afd0a6e31ed7bf
unknown <MochammadArdiansyahh>
2021-06-09 22:48:20 +0700

    berhasil

---
+++ src/study/kasus/pkg1/kedua.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package study.kasus.pkg1;
+
+/**
+ *
+ * @author Moch Ardiansyah
+ */
+public class kedua {
+    void belanja(){
+        String NamaBarang = "Kulkas";
+        int harga = 2000000;
+        
+        System.out.println("Nama Barang : "+NamaBarang);
+        System.out.println("Harga Barang : "+harga);
+    }
+}
